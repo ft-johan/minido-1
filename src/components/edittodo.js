2d4f1ff4a563cd695a65c37d7a4f455a7a4b1f81
Johan george <johangeorge201203@gmail.com>
2024-05-31 21:48:00 +0530

    Initial config

---
+++ src/components/edittodo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const edittodo = () => {
+  return (
+    <div>edittodo</div>
+  )
+}
---
+++ src/components/todoform.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const todoform = () => {
+  return (
+    <div>todoform</div>
+  )
+}
